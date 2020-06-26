d43a1071ac202dadb327d16a43b6ac99e33c8bdd
Sean Thames <sean.d.thames@gmail.com>
2020-06-26 19:53:54 -0400

    Uploading Get-LapsPwd.ps1
    Tool for querying the LAPS password from your domain controller. Replace [DOMAIN] on line 12 with your domain.

---
+++ LAPS/Get-LapsPwd.ps1
@@ -0,0 +1,37 @@
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$ComputerName
+)
+
+if($null -eq $AdminCreds) {
+    $AdminCreds = Get-Credential
+}
+
+
+Try {
+    (nltest /dsgetdc:[DOMAIN] /DS_6 /avoidself | findstr 'DC: ').split(" ") | %{
+
+        If($_ -like '\\*'){
+            #DC Found
+                $DC= $_.replace('\\','')
+        }
+    }
+}
+Catch {
+    $DC = $false #No DC found
+}
+
+
+If ($DC -and (Test-Connection $DC -Quiet -Count 1)){
+    #$DC exists
+    $CompObject = Invoke-Command -ComputerName $DC -ScriptBlock {
+        Get-ADComputer $using:ComputerName -Property ms-Mcs-AdmPwd
+    } -Credential $AdminCreds
+    Write-Output "Local Admin for $ComputerName :"
+    Write-Host -ForegroundColor yellow "`t", $CompObject.'ms-Mcs-AdmPwd'
+}
+Else{
+    #$DC is $false
+        write-host "Nearest Domain controller not found!"
+}
+
